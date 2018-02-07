1747987df8035249d964ec9586763125431871b5
Caleb Adrian <calebadrian1847@gmail.com>
2018-02-07 15:22:07 -0700

    Draws all elements to page with functionality

--- itunes-controller.js
+++ itunes-controller.js
@@ -8,8 +8,37 @@ function ItunesController(){
   }
 
   //Start coding here
-  function draw(results){
-    console.log(results)
+  function draw(songList){
+    var songsContainer = document.getElementById('songs-container')
+    var template = ''
+    for (var song in songList){
+      if (song == 9){break;}
+      if (song%3 == 0){
+        template += `<div class="row m-b-1">
+                      <div class = "card-deck">`
+      }
+      template += `
+          <div class="card bg-card text-center" style="width: 20rem">
+            <img class="resize img-thumbnail" src="${songList[song].albumArt}" alt="">
+            <div class="card-body">
+              <h5 class="card-title">${songList[song].title}</h5>
+              <p class="">Artist: ${songList[song].artist}</p>
+              <p class="">Album: ${songList[song].collection}</p>
+              <p class="">Price: $${songList[song].price}</p>
+            </div>
+            <div class="card-footer remove-padding">
+              <audio controls>
+                <source src="${songList[song].preview}">
+              </audio>
+            </div>
+          </div>
+      `
+      if (song == 2 || song == 5 || song == 8){
+        template += `</div>
+                  </div>`
+      }
+    }
+    songsContainer.innerHTML = template
   }
 
 
